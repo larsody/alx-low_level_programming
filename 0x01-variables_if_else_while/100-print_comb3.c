aedfd763d36f2505e158ea12fcba3ce8a86d64e2
Kayode Odeyemi <larsody@gmail.com>
2022-06-03 16:09:02 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,7 @@
 int main(void)
 {
 int n, m;
-for (n = 48; n <=56; n++)
+for (n = 48; n <= 56; n++)
 {
 for (m = 49; m <= 57; m++)
 {
@@ -22,7 +22,7 @@ if (m > n)
 {
 putchar(n);
 putchar(m);
-if (n != 56 || m !=57)
+if (n != 56 || m != 57)
 {
 putchar(',');
 putchar(' ');
