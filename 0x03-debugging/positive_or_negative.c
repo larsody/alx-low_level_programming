d09df1b81225ca8c6a8fa4417b2ed21ff955f7d4
Kayode Odeyemi <larsody@gmail.com>
2022-06-08 14:14:14 -0700

    My first debugging task

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,6 +3,7 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 int i;
@@ -10,15 +11,34 @@ i = 98;
 positive_or_negative(i);
 return (0);
 }
+
+
 /**
  * positive_or_negative - checks and prints numbers positive or negative
- * Return: void
- */
+ *
+ *
+ *    * Return: void
+ *
+ *    */
+
+
+
 void positive_or_negative(int i)
+
 {
-if (i > 0)
-{
-printf("%d is positive\n", i);
-} else if (i < 0)
-printf("%d is negative\n", i);
+
+		if (i > 0)
+
+				{
+
+							printf("%d is positive\n", i);
+
+								} else if (i < 0)
+
+											printf("%d is negative\n", i);
+
 }
+
+© 2022 GitHub, Inc.
+
+
