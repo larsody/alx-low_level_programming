fdd6cc8a4eb55618b4ad15cee28cad26cd9b7e2b
Kayode Odeyemi <larsody@gmail.com>
2022-06-09 09:37:46 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,14 @@
  */
 void print_numbers(void)
 {
-char n = 0;
-while (n <= 9)
-{
-_putchar('0' + n);
-n++;
-}
-_putchar('\n');
+	char n = 0;
+
+	while (n <= 9)
+	{
+		_putchar('0' + n);
+		n++;
+
+	}
+	_putchar('\n');
+
 }
