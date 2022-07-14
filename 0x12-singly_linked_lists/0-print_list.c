813925f0bf8621c1b0fbec6e95201df8ce7fecd6
larsody <larsody@gmail.com>
2022-07-14 12:46:04 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t num_nodes;
+	size_t num_nodes = 0;
+	const list_t *current = h;
 
-	num_nodes = 0;
-	while (h != NULL)
+	while (current != NULL)
 	{
-		if (h->str == NULL)
+		if (current->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
 		num_nodes++;
 	}
 	return (num_nodes);
