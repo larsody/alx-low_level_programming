dbe6580837139bc24c84611fa1aaf2c09c4033b3
larsody <larsody@gmail.com>
2022-07-18 16:37:06 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,6 @@
  * @h: singly linked list.
  * Return: number of nodes in the list.
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,17 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-
 #include <stdlib.h>
-
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
@@ -20,6 +15,5 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-
 
 #endif
