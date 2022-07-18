88078aa8773f052f5d84aeaa0ba105b312c9a94c
larsody <larsody@gmail.com>
2022-07-18 16:46:20 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listnt_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t num_element = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		num_element++;
+	}
+	return (num_element);
+}
