f80fd167565eded528732073fed46b93db7416cc
larsody <larsody@gmail.com>
2022-07-19 02:24:45 -0700

    advanced task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,72 @@
+#include "lists.h"
+
+/**
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *tmp;
+	listp_t *current;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while ((tmp = current) != NULL)
+		{
+			current = current->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t new_node = 0;
+	listp_t *head_ptr, *new, *add;
+	listint_t *current;
+
+	head_ptr = NULL;
+	while (*h != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = head_ptr;
+		head_ptr = new;
+
+		add = head_ptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&head_ptr);
+				return (new_node);
+			}
+		}
+
+		current = *h;
+		*h = (*h)->next;
+		free(current);
+		new_node++;
+	}
+
+	*h = NULL;
+	free_listp2(&head_ptr);
+	return (new_node);
+}
